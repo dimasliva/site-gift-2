2b9727b15f1c0b611893a744f35fdd17a0dd04a7
ermilov.dima <ermilovdima49@gmail.com>
2024-02-23 10:54:59 +0500

    initial

---
+++ src/App.vue
@@ -0,0 +1,265 @@
+<template>
+  <div>
+        <!-- Nav Bar Start -->
+        <div class="navbar navbar-expand-lg bg-light navbar-light">
+            <div class="container-fluid">
+                <a href="index.html" class="navbar-brand">Подарок</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
+
+
+        <!-- Hero Start -->
+        <div class="hero" id="home">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-sm-12 col-md-6">
+                        <div class="hero-content">
+                            <div class="hero-text">
+                                <p>Для</p>
+                                <h1>Юли Белковой</h1>
+                                <h2></h2>
+                                <div class="typed-text">Web Designer, Web Developer, Front End Developer, Apps Designer, Apps Developer</div>
+                            </div>
+                            <!-- <div class="hero-btn">
+                                <a class="btn" href="">Hire Me</a>
+                                <a class="btn" href="">Contact Me</a>
+                            </div> -->
+                        </div>
+                    </div>
+                    <div class="col-sm-12 col-md-6 d-none d-md-block">
+                        <div class="hero-image">
+                            <img src="./assets/img/hero.png" alt="Hero Image">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero End -->
+
+
+        <!-- About Start -->
+        <div class="about wow fadeInUp" data-wow-delay="0.1s" id="about">
+            <div class="container-fluid">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="about-img">
+                            <img src="./assets/img/about.jpg" alt="Image">
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="about-content">
+                            <div class="section-header text-left">
+                                <p>Лучшей подруге</p>
+                                <h2>10 лет вместе</h2>
+                            </div>
+                            <div class="about-text">
+                                <p>
+                                  Юля - наш огонь во всем этом холоде! Она как шторм на море - всегда в центре внимания, всегда полна энергии и всегда заставляет нас улыбаться. Ее смех - это настоящий музыкальный аккомпанемент жизни, ее шутки - это настоящий артхаус комедийного жанра. С Юлей никогда не бывает скучно, ведь она как бутылка шампанского - всегда готова взорваться эмоциями и радостью!
+                                </p>
+                            </div>
+                            <div class="skills">
+                                <div class="skill-name">
+                                    <p>Чувство юмора</p><p>85%</p>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar one" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <div class="skill-name">
+                                    <p>Занудство</p><p>0%</p>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar two" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <div class="skill-name">
+                                    <p>Смех</p><p>100%</p>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar three" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <div class="skill-name">
+                                    <p>Любит поесть</p><p>50%</p>
+                                </div>
+                                <div class="progress">
+                                    <div class="progress-bar four" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+                            <!-- <a class="btn" href="">Learn More</a> -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+        
+        
+        <!-- Service Start -->
+        <div class="service" id="service">
+            <div class="container">
+                <div class="section-header text-center wow zoomIn" data-wow-delay="0.1s">
+                    <p>В чём</p>
+                    <h2>Лучше всех</h2>
+                </div>
+                <div class="row">
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.0s">
+                        <div class="service-item">
+                            <div class="service-icon">
+                              <i class="fa fa-solid fa-dice"></i>
+                            </div>
+                            <div class="service-text">
+                                <h3>Настолках</h3>
+                                <p>
+                                  Хорошо играет в настольные игры. Внимательна к правилам, стратегичена, умеет анализировать ситуацию и принимать обдуманные решения. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="service-item">
+                            <div class="service-icon">
+                              <i class="fa fa-regular fa-gamepad"></i>
+                            </div>
+                            <div class="service-text">
+                                <h3>Играх</h3>
+                                <p>
+                                  Обладает отличной координацией движений, быстрым реагированием на изменения в игровой ситуации и стратегическим мышлением.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="service-item">
+                            <div class="service-icon">
+                                <i class="fa fa-heart"></i>
+                            </div>
+                            <div class="service-text">
+                                <h3>Любви</h3>
+                                <p>
+                                  Умеет любить, проявляя заботу, поддержку, уважение и преданность к другому человеку. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="service-item">
+                            <div class="service-icon">
+                              <i class="fa fa-solid fa-brain"></i>
+                            </div>
+                            <div class="service-text">
+                                <h3>Сообразительности</h3>
+                                <p>
+                                  Обладает широким кругозором, быстрым умом и способен легко анализировать и решать сложные проблемы. 
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Service End -->
+        
+   
+        <!-- Price End -->
+        
+        
+        <!-- Testimonial Start -->
+        <div class="testimonial wow fadeInUp" data-wow-delay="0.1s" id="review">
+            <div class="container">
+                <div class="testimonial-icon">
+                    <i class="fa fa-quote-left"></i>
+                </div>
+                <div class="owl-carousel testimonials-carousel">
+                    <div class="testimonial-item">
+                        <div class="testimonial-img">
+                            <img src="./assets/img/testimonial-1.jpg" alt="Image">
+                        </div>
+                        <div class="testimonial-text">
+                            <p>
+                              С днём рождения тебя, моя дорогая подруга! Пусть этот день будет наполнен радостью, улыбками и приятными сюрпризами. Желаю тебе счастья, любви, здоровья и успехов во всех начинаниях. Ты заслуживаешь только самого лучшего! Пусть жизнь приносит тебе лишь радость и удовлетворение.
+                            </p>
+                            <h3>Маша Светлова</h3>
+                            <h4>Подруга</h4>
+                        </div>
+                    </div>
+                    <div class="testimonial-item">
+                        <div class="testimonial-img">
+                            <img src="./assets/img/testimonial-2.jpg" alt="Image">
+                        </div>
+                        <div class="testimonial-text">
+                            <p>
+                              Моя дорогая жена, сегодня хочу поздравить тебя с твоим днем рождения! Ты - моя вдохновляющая, нежная и заботливая половинка, и я благодарен за то, что ты есть в моей жизни. Пусть этот день будет полон радости, улыбок и приятных сюрпризов. Я люблю тебя больше всего на свете и всегда буду рядом, чтобы делить с тобой все радости и трудности жизни. С днем рождения, моя дорогая! 🎉❤️
+                            </p>
+                            <h3>Олег Малинов</h3>
+                            <h4>Муж</h4>
+                        </div>
+                    </div>
+                    <div class="testimonial-item">
+                        <div class="testimonial-img">
+                            <img src="./assets/img/testimonial-3.jpg" alt="Image">
+                        </div>
+                        <div class="testimonial-text">
+                            <p>
+                              Дорогая Юля,
+
+                              От имени всей нашей компании я хочу поздравить тебя с твоим днем рождения! Желаю тебе крепкого здоровья, личного счастья, успехов в работе и во всех начинаниях. Твоя преданность и профессионализм важны для нашей команды, и мы ценим твою работу. Пусть этот день будет наполнен яркими моментами, радостью и улыбками!
+                            </p>
+                            <h3>Света Шурина</h3>
+                            <h4>Директор</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Testimonial End -->
+
+        <!-- Footer Start -->
+        <div class="footer wow fadeIn" data-wow-delay="0.3s">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="footer-info">
+                        <h2>Поздравляем</h2>
+                        <h3>Юлю Белкову</h3>
+                        <div class="footer-menu">
+                            <p>Всего</p>
+                            <p>Лучшего</p>
+                        </div>
+                        <div class="footer-social">
+                            <a href=""><i class="fab fa-telegram"></i></a>
+                            <a href=""><i class="fab fa-vk"></i></a>
+                            <a href=""><i class="fab fa-youtube"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="container copyright">
+                    <p></p>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+        
+        
+        <!-- Back to top button -->
+        <a href="#" class="btn back-to-top"><i class="fa fa-chevron-up"></i></a>
+        
+        
+        <!-- Pre Loader -->
+        <div id="loader" class="show">
+            <div class="loader"></div>
+        </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+@import url("@/assets/css/style.css");
+</style>
